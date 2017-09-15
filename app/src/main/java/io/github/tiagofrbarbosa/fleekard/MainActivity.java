edb314853dfda1beaae2d9d592694aab7b7850e1
Tiago Barbosa <tfreirebar@gmail.com>
2017-09-15 19:55:26 -0300

    feat: fleekard init

---
+++ app/src/main/java/io/github/tiagofrbarbosa/fleekard/MainActivity.java
@@ -0,0 +1,17 @@
+package io.github.tiagofrbarbosa.fleekard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tfbarbosa on 15/09/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
