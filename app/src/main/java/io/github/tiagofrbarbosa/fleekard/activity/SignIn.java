fab1fd5c8a776dff7c661602f732325c161aecf3
Tiago Barbosa <tfreirebar@gmail.com>
2017-09-24 17:39:57 -0300

    feat: sigin

--- app/src/main/java/io/github/tiagofrbarbosa/fleekard/activity/SignIn.java
+++ app/src/main/java/io/github/tiagofrbarbosa/fleekard/activity/SignIn.java
@@ -1,11 +1,9 @@
 package io.github.tiagofrbarbosa.fleekard.activity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
