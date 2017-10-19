2ce4036e288e101255fa5ca500a581630d58d774
Tiago Barbosa <tfreirebar@gmail.com>
2017-10-18 23:50:24 -0200

    feat: add notifications on listview

--- app/src/main/java/io/github/tiagofrbarbosa/fleekard/widget/WidgetDataProvider.java
+++ app/src/main/java/io/github/tiagofrbarbosa/fleekard/widget/WidgetDataProvider.java
@@ -7,8 +7,22 @@ import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import io.github.tiagofrbarbosa.fleekard.FleekardApplication;
+import io.github.tiagofrbarbosa.fleekard.firebaseConstants.Database;
+import io.github.tiagofrbarbosa.fleekard.model.Notification;
+import io.github.tiagofrbarbosa.fleekard.model.User;
+import timber.log.Timber;
 
 /**
  * Created by tfbarbosa on 18/10/17.
@@ -36,8 +50,59 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     }
 
     private void initData(){
-        mCollections.clear();
-        for(int i=1;i<=10;i++) mCollections.add("Text: " + i);
+
+        FirebaseUser mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Timber.tag("widgetFirebase").e(mFirebaseUser.getDisplayName());
+
+        DatabaseReference mUserReference = FirebaseDatabase.getInstance().getReference()
+                .child(Database.users.CHILD_USERS);
+
+        mUserReference
+                .orderByChild(Database.users.USER_ID)
+                .equalTo(mFirebaseUser.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot userSnap : dataSnapshot.getChildren()){
+                            User mUser = userSnap.getValue(User.class);
+                            Timber.tag("widgetFirebase").e(mUser.getUserKey());
+                            setWidgetNotifications(mUser.getUserKey());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    public void setWidgetNotifications(String userKey){
+        DatabaseReference mNotificationReference = FirebaseDatabase.getInstance().getReference()
+                .child(Database.notification.CHILD_NOTIFICATION)
+                .child(userKey);
+
+        mNotificationReference
+                .orderByChild(Database.notification.USER_KEY_NOTIFICATE)
+                .equalTo(userKey)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(mCollections != null) mCollections.clear();
+
+                        for(DataSnapshot notificationSnap : dataSnapshot.getChildren()){
+                            Notification notification = notificationSnap.getValue(Notification.class);
+                            mCollections.add(String.valueOf(notification.getTimeStampLong()));
+                            Timber.tag("widgetFirebase").e(String.valueOf(notification.getTimeStampLong()));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     @Override
